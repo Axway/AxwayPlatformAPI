20afc8c243877a5ec8f0ef4932767de32f2fb356
Hristina Stoykova <hstoykova@axway.com>
2025-01-09 06:37:27 -0700

    Update

---
+++ README.md
@@ -0,0 +1,22 @@
+# UsageReportingToPlatform
+
+
+## About
+This is a sample script to upload ST usage metrics to the Amplify Platform. It is intended to be used in the event that the ST server resides in a network saegment with no outbound connectivity to internet. 
+
+This is intended to be an example and Axway provides no support or guarantees. Please review, test, and update as needed prior to using in PROD. 
+
+It will generate a full report for the previous calendar 
+## Axway documentation
+
+### Axway Platform REST API specification
+https://platform.axway.com/api-docs.html
+
+### Usage Reporting Configuration in ST
+https://docs.axway.com/bundle/SecureTransport_55_AdministratorGuide_allOS_en_HTML5/page/Content/AdministratorsGuide/setup/create-usage-reports.htm
+
+### SecureTransport REST API specification (admin)
+https://apidocs.axway.com/swagger-ui-st/admin-20/
+
+
+
--- platformAuth.py
+++ platformAuth.py
@@ -1,94 +1,3 @@
-# import json
-# import os
-# import logging
-# from datetime import date, timedelta
-# from dateutil.relativedelta import relativedelta
-# import requests
-# import dotenv
-# from funcsession import Authenticate
-#
-# # Load environment variables
-# dotenv_path = ".env"
-# dotenv.load_dotenv(dotenv_path)
-#
-# # Constants
-# URL = "https://platform.axway.com/api/v1/"
-# URL_TOKEN = 'https://login.axway.com/auth/realms/Broker/protocol/openid-connect/token'
-# ORG_ID = os.getenv("ORG_ID")
-# PLATFORM_CLIENT = os.getenv("PLATFORM_CLIENT")
-# PLATFORM_KEY = os.getenv("PLATFORM_KEY")
-#
-# # Get current date
-# today = date.today()
-#
-# # Get first and last day of the previous month
-# first_day = today.replace(day=1) - relativedelta(months=1)
-# last_day = today.replace(day=1) - timedelta(days=1)
-#
-# # Format dates
-# start = first_day.strftime('%d/%m/%Y')
-# end = last_day.strftime('%d/%m/%Y')
-#
-# # Get access token
-# data = {
-#     "grant_type": "client_credentials",
-#     "client_id": PLATFORM_CLIENT,
-#     "client_secret": PLATFORM_KEY
-# }
-#
-# # Authenticate and generate report
-# s, urlST = Authenticate('USER_GROUP')
-#
-# response = requests.post(URL_TOKEN, data=data)
-# response.raise_for_status()  # Check for request errors
-# token_payload = response.json()
-#
-# # Set headers and parameters
-# headers = {
-#     'Authorization': f"Bearer {token_payload['access_token']}",
-#     "Content-Type": "application/json"
-# }
-# params = {'org_id': ORG_ID}
-#
-# # Fetch usage data
-# resource = 'usage/'
-# response = requests.get(URL + resource, headers=headers, params=params)
-# response.raise_for_status()  # Check for request errors
-# if response.ok:
-#     logging.info("Got current stats: %s", response.json())
-#
-# report_filename = f"usage-report-last-month-{today}.json"
-# resource = "statisticsSummary/generateReport/"
-# params = {
-#     'startDate': start,
-#     'endDate': end,
-#     'includeTransfersIn': True,
-#     'includeActiveUsersCount': True,
-#     'includeIncomingFileVolume': True
-# }
-#
-# response = s.get(urlST + resource, params=params)
-# response.raise_for_status()  # Check for request errors
-#
-# # Save report to file
-# os.makedirs("Reports", exist_ok=True)
-# with open(f"Reports/{report_filename}", "w") as report:
-#     json.dump(response.json(), report, indent=4)
-#
-# # Upload report
-# resource = 'usage/'
-# payload = {'uploadMethod': 'automatic'}
-# files = [
-#     ('file', (report_filename, open(f'Reports/{report_filename}', 'rb'), 'application/json'))
-# ]
-# headers.pop('Content-Type', None)  # Remove Content-Type header for file upload
-#
-# response = requests.post(URL + resource, headers=headers, data=payload, files=files)
-# if response.ok:
-#     logging.info("Successfully submitted report to the Axway Platform")
-# else:
-#     logging.error("Error submitting report: %s", response.text)
-
 import json
 import os
 import logging
@@ -100,7 +9,7 @@ from funcsession import Authenticate
 
 def main():
     # Load environment variables
-    dotenv_path = ".env"
+    dotenv_path = "conf/.env"
     dotenv.load_dotenv(dotenv_path)
 
     # Constants
